2f85370a956520a0ac15ddb2c52b83c8f76dfd5e
Holly Brice <hbrice@uoregon.edu>
2014-06-01 10:48:49 -0700

    database info

---
+++ client/js/database.js
@@ -0,0 +1,10 @@
+/* Restaurant entry for DB */
+var query = {"restaurant_name": "Caspian", "meal_time": ["breakfast", "lunch", "dinner"], "food_category": ["mediterranean", "american"], "price": "min", "goal_meal": ["egg", "curry", "salad", "quesadilla", "burger", "gyro", "falafel", "burrito"]};  //add for testing
+
+{"restaurant_name": "Rennies_Landing", "meal_time": ["breakfast", "lunch", "dinner"], "food_category": ["pubfood", "american"], "price": "min", "goal_meal": ["egg", ,"chicken", "gardenburger", "steak", "nacho", "hotwing" "pancake", "salad", "quesadilla", "burger", "gyro", "falafel", "burrito"]};
+{"restaurant_name": "Qdoba", "meal_time": ["breakfast", "lunch", "dinner"], "food_category": ["mexican"], "price": "min", "goal_meal": ["egg", ,"burrito", "nacho", "gumbo" "taco", "salad", "quesadilla", "chip"]};
+{"restaurant_name": "East_Meets_West", "meal_time": ["breakfast", "lunch", "dinner"], "food_category": ["asian"], "price": "min", "goal_meal": [""]};
+{"restaurant_name": "Glenwood", "meal_time": ["breakfast", "lunch", "dinner"], "food_category": ["american"], "price": "med", "goal_meal": ["egg", ,"burrito"]};
+{"restaurant_name": "Webfoot_Bar_and_Grill", "meal_time": ["breakfast", "lunch", "dinner"], "food_category": ["american"], "price": "med", "goal_meal": ["egg", ,"burrito"]};
+{"restaurant_name": "Alder_Stree_Fish_Co", "meal_time": ["lunch", "dinner"], "food_category": ["american"], "price": "med", "goal_meal": ["fish_and_chip", ,"burrito"]};
+{"restaurant_name": "Sweet_Basil_Express", "meal_time": ["lunch", "dinner"], "food_category": ["thai"], "price": "med", "goal_meal": ["fish_and_chip", ,"burrito"]};
